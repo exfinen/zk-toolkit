8d2c7b8010cc162028a12088508de99ebbde25f7
Kazumune Masaki <kazumune.masaki@bitcoinbank.co.jp>
2022-02-25 18:54:47 +0900

    wip

--- src/field.rs
+++ src/field.rs
@@ -3,6 +3,7 @@ use num_bigint::BigUint;
 pub struct Field {
   order: BigUint,
   zero: BigUint,
+  one: BigUint,
 }
 
 pub struct FieldNum<'a> {
@@ -12,7 +13,11 @@ pub struct FieldNum<'a> {
 
 impl Field {
   pub fn new(order: BigUint) -> Self {
-    Field { order, zero: BigUint::from(0u32) }
+    Field { 
+      order, 
+      zero: BigUint::from(0u32),
+      one: BigUint::from(1u32),
+    }
   }
 
   pub fn gen_element(&self, v: BigUint) -> FieldNum {
@@ -48,6 +53,41 @@ impl <'a> FieldNum<'a> {
     } else {
       let v = self.v.clone() - other.v.clone();
       FieldNum { f: self.f, v }
+    }
+  }
+
+  pub fn mul(&self, other: &FieldNum) -> FieldNum {
+    let v = (self.v.clone() * other.v.clone()) % self.f.order.clone();
+    if v >= self.f.zero {
+      FieldNum { f: self.f, v }
+    } else {
+      let v = self.f.order.clone() + v.clone();
+      FieldNum { f: self.f, v }
+    }
+  }
+
+  pub fn inverse(&self) -> Result<FieldNum, String> {
+    if (self.v == self.f.zero) {
+      return Err("division by zero".to_string());
+    }
+    let mut t = self.f.zero.clone();
+    let mut r = self.f.order.clone();
+    let mut nt = self.f.one.clone();
+    let mut nr = self.v.clone() % self.f.order.clone();
+
+    while nr != self.f.zero {
+      let q = r / nr.clone();
+      t = nt.clone();
+      nt = t.clone() - q.clone() * nt;
+      r = nr.clone();
+      nr = r.clone() - q * nr;
+    }
+
+    if t < self.f.zero {
+      let v = t + self.f.order.clone();
+      Ok(FieldNum { f: self.f, v })
+    } else {
+      Ok(FieldNum { f: self.f, v: t })
     }
   }
 }
@@ -108,5 +148,40 @@ mod tests {
     let b = f.gen_element(BigUint::from(10u32));
     let c = a.sub(&b);
     assert_eq!(c.v, BigUint::from(10u32));
+  }
+
+  #[test]
+  fn test_mul_below_order_result() {
+    let f = Field::new(BigUint::from(11u32));
+    let a = f.gen_element(BigUint::from(2u32));
+    let b = f.gen_element(BigUint::from(5u32));
+    let c = a.mul(&b);
+    assert_eq!(c.v, BigUint::from(10u32));
+  }
+
+  #[test]
+  fn test_mul_eq_order_result() {
+    let f = Field::new(BigUint::from(11u32));
+    let a = f.gen_element(BigUint::from(1u32));
+    let b = f.gen_element(BigUint::from(11u32));
+    let c = a.mul(&b);
+    assert_eq!(c.v, BigUint::from(0u32));
+  }
+
+  #[test]
+  fn test_mul_above_order_result() {
+    let f = Field::new(BigUint::from(11u32));
+    let a = f.gen_element(BigUint::from(1u32));
+    let b = f.gen_element(BigUint::from(11u32));
+    let c = a.mul(&b);
+    assert_eq!(c.v, BigUint::from(0u32));
+  }
+
+  #[test]
+  fn test_inverse() -> Result<(), String> {
+    let f = Field::new(BigUint::from(11u32));
+    let a = f.gen_element(BigUint::from(3u32));
+    assert_eq!(a.inverse()?.v, BigUint::from(0u32));
+    Ok(())
   }
 }
