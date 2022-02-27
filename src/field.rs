78974519b761f96e00400a6c98c6fbe069f3acf1
Kazumune Masaki <exfinen@gmail.com>
2022-02-27 16:24:36 +0900

    clean up and add a test case

--- src/field.rs
+++ src/field.rs
@@ -70,25 +70,21 @@ impl <'a> FieldNum<'a> {
     if self.v == self.f.zero {
       return Err("cannot find inverse of zero".to_string());
     }
+    let order = self.f.order.to_bigint().unwrap();
+    let v = self.v.to_bigint().unwrap();
     let zero = self.f.zero.to_bigint().unwrap();
     let one = self.f.one.to_bigint().unwrap();
 
-    println!("self.v={}", self.v);
-
     // x0*a + y0*b = a
     // x1*a + y1*b = b
-    // let mut r0 = self.v.to_bigint().unwrap();  // b
-    // let mut r1 = self.f.order.to_bigint().unwrap();  // a
-    let mut r0 = self.f.order.to_bigint().unwrap();  // a
-    let mut r1 = self.v.to_bigint().unwrap();  // b
+    let mut r0 = v.clone();  // initially equals to a
+    let mut r1 = order.clone();  // initially equals to b
     let mut x0 = one.clone();
     let mut y0 = zero.clone();
     let mut x1 = zero.clone();
     let mut y1 = one.clone();
 
     while r1 != zero {
-      println!("x0={}, y0={}, r0={}", x0, y0, r0);
-      println!("x1={}, y1={}, r1={}", x1, y1, r1);
       // a mod b
       // = a - q*b
       // = (x0*a + y0*b) - q*(x1*a + y1*b)
@@ -97,12 +93,10 @@ impl <'a> FieldNum<'a> {
       // = r
       let q = r0.clone() / r1.clone();
       let r2 = r0.clone() % r1.clone();
+      // this produces the same result as above r2 using mod
+      //let r2 = x2.clone() * order.clone() + y2.clone() * v.clone();
       let x2 = x0 - x1.clone() * q.clone();
       let y2 = y0 - y1.clone() * q.clone();
-      let r2a = x2.clone() * self.f.order.to_bigint().unwrap() + y2.clone() * self.v.to_bigint().unwrap();
-      println!("q={}, x2={}, y2={}", q, x2, y2);
-
-      println!("new r={}, calc-r={}", r2, r2a);
 
       // do next calculattion based on new and previous equations
       r0 = r1;
@@ -113,15 +107,19 @@ impl <'a> FieldNum<'a> {
       y1 = y2;
     }
 
-    println!("result x = {}", x0); // last eq's x when r became 0
-
-
-    if r1 < zero {
-      let v = r1 + self.f.order.to_bigint().unwrap();
-      Ok(FieldNum { f: self.f, v: v.to_biguint().unwrap() }) // TODO use loop
+    //println!("x0={}, x1={}", x0, x1);
+    // if the result is not field element, convert it to field element
+    let mut new_v = x0;
+    if new_v < zero.clone() {
+      while new_v < zero.clone() {
+        new_v += order.clone();
+      }
     } else {
-      Ok(FieldNum { f: self.f, v: r1.to_biguint().unwrap() })
+      if new_v >= order {
+        new_v %= order;
+      }
     }
+    Ok(FieldNum { f: self.f, v: new_v.to_biguint().unwrap() })
   }
 }
 
@@ -210,11 +208,26 @@ mod tests {
     assert_eq!(c.v, BigUint::from(0u32));
   }
 
+  struct InvTestCase {
+    order: u32,
+    v: u32,
+    exp: u32,
+  }
+
   #[test]
-  fn test_inv() -> Result<(), String> {
-    let f = Field::new(BigUint::from(53u32));
-    let a = f.gen_element(BigUint::from(97u32));
-    assert_eq!(a.inv()?.v, BigUint::from(0u32));
+  fn test_inv1() -> Result<(), String> {
+    let test_cases = [
+      InvTestCase { order: 53u32, v: 97u32, exp: 47u32 },
+      InvTestCase { order: 13u32, v: 5u32, exp: 8u32 },
+    ];
+
+    for x in test_cases {
+      let f = Field::new(BigUint::from(x.order));
+      let a = f.gen_element(BigUint::from(x.v));
+      let inv = a.inv()?;
+      //println!("inv of {} (mod {}): exp={}, act={}", x.v, x.order, x.exp, inv.v);
+      assert_eq!(inv.v, BigUint::from(x.exp));
+    }
     Ok(())
   }
 }
