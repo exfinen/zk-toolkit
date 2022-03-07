28763f44e783bb851a6ed227c130dca3c6f6ad5e
kz <parumu4github@gmail.com>
2022-03-07 13:29:51 +0900

    wip

--- src/ec.rs
+++ src/ec.rs
@@ -1,9 +1,12 @@
 use super::field::{Field, FieldNum};
 use num_bigint::BigUint;
 
+// represents: y^2 = x^3 + Ax + B
 #[allow(dead_code)]
-pub struct Ec<'a> {
+pub struct WeierstrassEquation<'a> {
   f: &'a Field,
+  A: FieldNum<'a>,
+  B: FuekdNum<'a>,
 }
 
 #[allow(dead_code)]
@@ -13,29 +16,42 @@ pub struct EcPoint<'a> {
   y: FieldNum<'a>,
 }
 
-impl <'a> Ec <'a> {
+impl <'a> WeierstrassEquation <'a> {
   #[allow(dead_code)]
-  pub fn new(f: &'a Field, _p: BigUint) -> Self {
-    Ec {
-      f,
-    }
+  pub fn new(f: &'a Field, A: BigUint, B: BigUint) -> Self {
+    WeierstrassEquation { f, A, B }
   }
 
   #[allow(dead_code)]
   pub fn add(&self, p1: &'a EcPoint, p2: &'a EcPoint) -> EcPoint<'a> {
-    // for now, assumes that p1 != p2
+    // for now, this code assumes that p1 != p2
 
     // equation of the line that intersects the curve at p1 and p2:
-    // y = m(x − x_1) + y_1
+    // p2.y = m(p2.x − p1.x) + p1.y
+    // p2.y - p1.y = m(p2.x - p1.x)
+    // m
     // (m(x − x_1) + y_1)^2 = x^3 + Ax + B  // substitute the y of Wirestrass eq w/ above
     // 0 = x^3 - m^2 x^2 + ...   // move LHS to RHS
     //
-    // using below equation, x-coordinate of the 3rd point can be calculated
+    // using below equation, x-coordinate of the 3rd point can be calculated.
     // r and s are the x-coordinates of p1 and p2, and t is the x-coordinate 
-    // of the 3rd point
-    // x^3 + ax^2 + bx + c = (x-r)(x-s)(x-t) = x^3 - (r+s+t)x^2 ...
-    // r + s + t = -a
-    let m = (p2.x.sub(&p1.y)).div(&p2.x.sub(&p1.x)).unwrap();
+    // of the 3rd point. since curve's x^3's coefficient is 1, this can be used.
+    //
+    // x^3 + Ax^2 + Bx + c = (x - p1.x)(x - p2.x)(x - p3.x) = x^3 - (p1.x + p2.x + p3.x)x^2 ...
+    // p1.x + p2.x + p3.x = -A
+    // p1.x + p2.x + A = -p3.x
+    // -1 * (p1.x + p2.x + A) = p3.x
+    let t = -1.mul(&(p1.x.add(&p2.x).add(self.A)));
+    
+    // then substitute x of the line's eq w/ thex-coordinate of the 3rd point 
+    // to calculate the y-coordinate and flip the point in terms of the x-axis
+    // 
+    // y = m(x − x_1) + y_1
+    
+    // slope of the line intesecting the curve
+    
+    let m = (p2.y.sub(&p1.y)).div(&p2.x.sub(&p1.x)).unwrap();
+    let rhs = m.mul(&p3.x).sub((m.mul(&p1.x))).add(&p1.y)
 
     let mm = m.mul(&m);
 
