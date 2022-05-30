8a07cd733bfbb814db6d3757065a612620c219b9
Kazumune Masaki <kazumune.masaki@bitcoinbank.co.jp>
2022-05-30 17:50:04 +0900

    specify K and initial hash value directly as numbers

--- src/sha256.rs
+++ src/sha256.rs
@@ -1,15 +1,8 @@
 #![allow(non_snake_case)]
-use hex::FromHex;
 use crate::hasher::Hasher;
 use crate::sha_common::{Block, MessageSchedule, HashValue, CoreLogic};
 
 // implementation based on: https://nvlpubs.nist.gov/nistpubs/FIPS/NIST.FIPS.180-4.pdf
-
-macro_rules! hex_to_u32 {
-  ($x:expr) => {
-    u32::from_be_bytes(<[u8; 4]>::from_hex($x).unwrap())
-  };
-}
 
 const BLOCK_SIZE: usize = 64;
 const DIGEST_SIZE: usize = 32;
@@ -52,31 +45,29 @@ impl<'a> CoreLogic<
 > for Sha256 {
 
   fn get_K() -> [u32; 64] {
-    let K: Vec<u32> = [
-      "428a2f98", "71374491", "b5c0fbcf", "e9b5dba5", "3956c25b", "59f111f1", "923f82a4", "ab1c5ed5",
-      "d807aa98", "12835b01", "243185be", "550c7dc3", "72be5d74", "80deb1fe", "9bdc06a7", "c19bf174",
-      "e49b69c1", "efbe4786", "0fc19dc6", "240ca1cc", "2de92c6f", "4a7484aa", "5cb0a9dc", "76f988da",
-      "983e5152", "a831c66d", "b00327c8", "bf597fc7", "c6e00bf3", "d5a79147", "06ca6351", "14292967",
-      "27b70a85", "2e1b2138", "4d2c6dfc", "53380d13", "650a7354", "766a0abb", "81c2c92e", "92722c85",
-      "a2bfe8a1", "a81a664b", "c24b8b70", "c76c51a3", "d192e819", "d6990624", "f40e3585", "106aa070",
-      "19a4c116", "1e376c08", "2748774c", "34b0bcb5", "391c0cb3", "4ed8aa4a", "5b9cca4f", "682e6ff3",
-      "748f82ee", "78a5636f", "84c87814", "8cc70208", "90befffa", "a4506ceb", "bef9a3f7", "c67178f2",
-    ].iter().map(|x| hex_to_u32!(x)).collect();
-    K.try_into().unwrap()
+    [
+      0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5, 0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5,
+      0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3, 0x72be5d74, 0x80deb1fe, 0x9bdc06a7, 0xc19bf174,
+      0xe49b69c1, 0xefbe4786, 0x0fc19dc6, 0x240ca1cc, 0x2de92c6f, 0x4a7484aa, 0x5cb0a9dc, 0x76f988da,
+      0x983e5152, 0xa831c66d, 0xb00327c8, 0xbf597fc7, 0xc6e00bf3, 0xd5a79147, 0x06ca6351, 0x14292967,
+      0x27b70a85, 0x2e1b2138, 0x4d2c6dfc, 0x53380d13, 0x650a7354, 0x766a0abb, 0x81c2c92e, 0x92722c85,
+      0xa2bfe8a1, 0xa81a664b, 0xc24b8b70, 0xc76c51a3, 0xd192e819, 0xd6990624, 0xf40e3585, 0x106aa070,
+      0x19a4c116, 0x1e376c08, 0x2748774c, 0x34b0bcb5, 0x391c0cb3, 0x4ed8aa4a, 0x5b9cca4f, 0x682e6ff3,
+      0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208, 0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2,
+    ]
   }
 
   fn get_initial_hash_value() -> HashValue<u32> {
-    let h: Vec<u32> = [
-      "6a09e667", 
-      "bb67ae85", 
-      "3c6ef372", 
-      "a54ff53a", 
-      "510e527f", 
-      "9b05688c", 
-      "1f83d9ab", 
-      "5be0cd19",
-    ].iter().map(|x| hex_to_u32!(x)).collect();
-    HashValue { h: h.try_into().unwrap() }
+    HashValue { h: [
+      0x6a09e667, 
+      0xbb67ae85, 
+      0x3c6ef372, 
+      0xa54ff53a, 
+      0x510e527f, 
+      0x9b05688c, 
+      0x1f83d9ab, 
+      0x5be0cd19,
+    ] }
   }
 }
 
--- src/sha512.rs
+++ src/sha512.rs
@@ -1,15 +1,8 @@
 #![allow(non_snake_case)]
-use hex::FromHex;
 use crate::hasher::Hasher;
 use crate::sha_common::{Block, MessageSchedule, HashValue, CoreLogic};
 
 // implementation based on: https://nvlpubs.nist.gov/nistpubs/FIPS/NIST.FIPS.180-4.pdf
-
-macro_rules! hex_to_u64 {
-  ($x:expr) => {
-    u64::from_be_bytes(<[u8; 8]>::from_hex($x).unwrap())
-  };
-}
 
 const BLOCK_SIZE: usize = 128;
 const DIGEST_SIZE: usize = 64;
@@ -56,43 +49,41 @@ impl<'a> CoreLogic<
 > for Sha512 {
 
   fn get_K() -> [u64; 80] { 
-    let K: Vec<u64> = [
-      "428a2f98d728ae22", "7137449123ef65cd", "b5c0fbcfec4d3b2f", "e9b5dba58189dbbc",
-      "3956c25bf348b538", "59f111f1b605d019", "923f82a4af194f9b", "ab1c5ed5da6d8118",
-      "d807aa98a3030242", "12835b0145706fbe", "243185be4ee4b28c", "550c7dc3d5ffb4e2",
-      "72be5d74f27b896f", "80deb1fe3b1696b1", "9bdc06a725c71235", "c19bf174cf692694",
-      "e49b69c19ef14ad2", "efbe4786384f25e3", "0fc19dc68b8cd5b5", "240ca1cc77ac9c65",
-      "2de92c6f592b0275", "4a7484aa6ea6e483", "5cb0a9dcbd41fbd4", "76f988da831153b5",
-      "983e5152ee66dfab", "a831c66d2db43210", "b00327c898fb213f", "bf597fc7beef0ee4",
-      "c6e00bf33da88fc2", "d5a79147930aa725", "06ca6351e003826f", "142929670a0e6e70",
-      "27b70a8546d22ffc", "2e1b21385c26c926", "4d2c6dfc5ac42aed", "53380d139d95b3df",
-      "650a73548baf63de", "766a0abb3c77b2a8", "81c2c92e47edaee6", "92722c851482353b",
-      "a2bfe8a14cf10364", "a81a664bbc423001", "c24b8b70d0f89791", "c76c51a30654be30",
-      "d192e819d6ef5218", "d69906245565a910", "f40e35855771202a", "106aa07032bbd1b8",
-      "19a4c116b8d2d0c8", "1e376c085141ab53", "2748774cdf8eeb99", "34b0bcb5e19b48a8",
-      "391c0cb3c5c95a63", "4ed8aa4ae3418acb", "5b9cca4f7763e373", "682e6ff3d6b2b8a3",
-      "748f82ee5defb2fc", "78a5636f43172f60", "84c87814a1f0ab72", "8cc702081a6439ec",
-      "90befffa23631e28", "a4506cebde82bde9", "bef9a3f7b2c67915", "c67178f2e372532b",
-      "ca273eceea26619c", "d186b8c721c0c207", "eada7dd6cde0eb1e", "f57d4f7fee6ed178",
-      "06f067aa72176fba", "0a637dc5a2c898a6", "113f9804bef90dae", "1b710b35131c471b",
-      "28db77f523047d84", "32caab7b40c72493", "3c9ebe0a15c9bebc", "431d67c49c100d4c",
-      "4cc5d4becb3e42b6", "597f299cfc657e2a", "5fcb6fab3ad6faec", "6c44198c4a475817",
-    ].iter().map(|x| hex_to_u64!(x)).collect();
-    K.try_into().unwrap()
+    [
+      0x428a2f98d728ae22, 0x7137449123ef65cd, 0xb5c0fbcfec4d3b2f, 0xe9b5dba58189dbbc,
+      0x3956c25bf348b538, 0x59f111f1b605d019, 0x923f82a4af194f9b, 0xab1c5ed5da6d8118,
+      0xd807aa98a3030242, 0x12835b0145706fbe, 0x243185be4ee4b28c, 0x550c7dc3d5ffb4e2,
+      0x72be5d74f27b896f, 0x80deb1fe3b1696b1, 0x9bdc06a725c71235, 0xc19bf174cf692694,
+      0xe49b69c19ef14ad2, 0xefbe4786384f25e3, 0x0fc19dc68b8cd5b5, 0x240ca1cc77ac9c65,
+      0x2de92c6f592b0275, 0x4a7484aa6ea6e483, 0x5cb0a9dcbd41fbd4, 0x76f988da831153b5,
+      0x983e5152ee66dfab, 0xa831c66d2db43210, 0xb00327c898fb213f, 0xbf597fc7beef0ee4,
+      0xc6e00bf33da88fc2, 0xd5a79147930aa725, 0x06ca6351e003826f, 0x142929670a0e6e70,
+      0x27b70a8546d22ffc, 0x2e1b21385c26c926, 0x4d2c6dfc5ac42aed, 0x53380d139d95b3df,
+      0x650a73548baf63de, 0x766a0abb3c77b2a8, 0x81c2c92e47edaee6, 0x92722c851482353b,
+      0xa2bfe8a14cf10364, 0xa81a664bbc423001, 0xc24b8b70d0f89791, 0xc76c51a30654be30,
+      0xd192e819d6ef5218, 0xd69906245565a910, 0xf40e35855771202a, 0x106aa07032bbd1b8,
+      0x19a4c116b8d2d0c8, 0x1e376c085141ab53, 0x2748774cdf8eeb99, 0x34b0bcb5e19b48a8,
+      0x391c0cb3c5c95a63, 0x4ed8aa4ae3418acb, 0x5b9cca4f7763e373, 0x682e6ff3d6b2b8a3,
+      0x748f82ee5defb2fc, 0x78a5636f43172f60, 0x84c87814a1f0ab72, 0x8cc702081a6439ec,
+      0x90befffa23631e28, 0xa4506cebde82bde9, 0xbef9a3f7b2c67915, 0xc67178f2e372532b,
+      0xca273eceea26619c, 0xd186b8c721c0c207, 0xeada7dd6cde0eb1e, 0xf57d4f7fee6ed178,
+      0x06f067aa72176fba, 0x0a637dc5a2c898a6, 0x113f9804bef90dae, 0x1b710b35131c471b,
+      0x28db77f523047d84, 0x32caab7b40c72493, 0x3c9ebe0a15c9bebc, 0x431d67c49c100d4c,
+      0x4cc5d4becb3e42b6, 0x597f299cfc657e2a, 0x5fcb6fab3ad6faec, 0x6c44198c4a475817,
+    ]
   }
 
   fn get_initial_hash_value() -> HashValue<u64> {
-    let h: Vec<u64> = [
-      "6a09e667f3bcc908",
-      "bb67ae8584caa73b",
-      "3c6ef372fe94f82b",
-      "a54ff53a5f1d36f1",
-      "510e527fade682d1",
-      "9b05688c2b3e6c1f",
-      "1f83d9abfb41bd6b",
-      "5be0cd19137e2179",
-    ].iter().map(|x| hex_to_u64!(x)).collect();
-    HashValue { h: h.try_into().unwrap() }
+    HashValue { h: [
+      0x6a09e667f3bcc908,
+      0xbb67ae8584caa73b,
+      0x3c6ef372fe94f82b,
+      0xa54ff53a5f1d36f1,
+      0x510e527fade682d1,
+      0x9b05688c2b3e6c1f,
+      0x1f83d9abfb41bd6b,
+      0x5be0cd19137e2179,
+    ] }
   }
 }
 
