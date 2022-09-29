d90cfe5a9dc5285442be18f35528a9d09534f63b
exfinen <47593166+exfinen@users.noreply.github.com>
2022-09-29 17:11:29 +0900

    compute solution vector w/ variables and a,b and c vectors for each gate

--- src/snarks/mod.rs
+++ src/snarks/mod.rs
@@ -1,5 +1,6 @@
 pub mod calculator;
 pub mod config;
+pub mod matrix;
 pub mod r1cs;
 pub mod equation_parser;
 pub mod gates;
--- src/snarks/r1cs.rs
+++ src/snarks/r1cs.rs
@@ -4,6 +4,7 @@ use std::collections::HashMap;
 use std::cmp::{PartialEq, Eq};
 
 use super::config::SignalId;
+use super::matrix::SparseVec;
 
 #[derive(Clone, Hash, PartialEq, Eq)]
 pub enum Term {
@@ -30,39 +31,83 @@ impl std::fmt::Debug for Term {
 }
 
 pub struct R1CS {
-  pub a: Vec<Vec<Term>>,
-  pub b: Vec<Vec<Term>>,
-  pub c: Vec<Vec<Term>>,
+  pub abcs: Vec<(SparseVec, SparseVec, SparseVec)>,
   pub w_tmpl: Vec<Term>,
   pub w_index: HashMap<Term, usize>,
 }
 
 impl R1CS {
-  pub fn build_template(gates: &[Gate]) -> R1CS {
-    let mut w_index = HashMap::<Term, usize>::new();
-    let mut w_tmpl: Vec<Term> = vec![Term::One];
+  fn new() -> Self {
+    let mut r1cs = R1CS {
+      abcs: vec![],
+      w_tmpl: vec![],
+      w_index: HashMap::<Term, usize>::new(),
+    };
+    r1cs.update_index(&Term::One);
+    r1cs
+  }
 
-    // TODO
-    // 1. don't add term if already in w
-    // 2. handle Sum properly
+  fn update_index(&mut self, t: &Term) {
+    match t {
+      Term::Sum(a, b) => {
+        self.update_index(&a);
+        self.update_index(&b);
+      },
+      t => {
+        if self.w_index.contains_key(t) { return };
+        self.w_tmpl.push(t.clone());
+        self.w_index.insert(t.clone(), self.w_index.len());
+      }
+    }
+  }
+
+  fn gate_to_vec(r1cs: &R1CS, f: &Field, vec: &mut SparseVec, term: &Term) {
+    match term {
+      Term::Sum(a, b) => {
+        R1CS::gate_to_vec(r1cs, f, vec, &a);
+        R1CS::gate_to_vec(r1cs, f, vec, &b);
+      },
+      x => {
+        let index = r1cs.w_index.get(&x).unwrap();
+        match &x {
+          Term::Num(n) => {
+            vec.set(*index, n.clone());
+          },
+          _ => {
+            vec.set(*index, f.elem(&1u8));
+          },
+        };
+      },
+    }
+  }
+
+  pub fn from_gates(f: &Field, gates: &[Gate]) -> R1CS {
+    let mut r1cs = R1CS::new();
+
+    // build witness vector
     for gate in gates {
-      w_tmpl.push(gate.a.clone());
-      w_index.insert(gate.a.clone(), gates.len());
-
-      w_tmpl.push(gate.b.clone());
-      w_index.insert(gate.b.clone(), gates.len());
-
-      w_tmpl.push(gate.c.clone());
-      w_index.insert(gate.c.clone(), gates.len());
+      r1cs.update_index(&gate.a);
+      r1cs.update_index(&gate.b);
+      r1cs.update_index(&gate.c);
     }
 
-    R1CS {
-      a: vec![vec![]],
-      b: vec![vec![]],
-      c: vec![vec![]],
-      w_tmpl,
-      w_index,
+    let vec_size = gates.len();
+
+    for gate in gates {
+      let mut a_vec = SparseVec::new(vec_size);
+      R1CS::gate_to_vec(&r1cs, f, &mut a_vec, &gate.a);
+
+      let mut b_vec = SparseVec::new(vec_size);
+      R1CS::gate_to_vec(&r1cs, f, &mut b_vec, &gate.b);
+
+      let mut c_vec = SparseVec::new(vec_size);
+      R1CS::gate_to_vec(&r1cs, f, &mut c_vec, &gate.c);
+
+      let abc = (a_vec, b_vec, c_vec);
+      r1cs.abcs.push(abc)
     }
+
+    r1cs
   }
 }
 
@@ -126,7 +171,7 @@ mod tests {
   #[test]
   fn test_gate_build() {
     let f = &Field::new(&3911u16);
-    let input = "3 * x + 4 = 10";
+    let input = "3 * x + 4";
     println!("Equation: {}", input);
     let (_, eq) = Parser::parse(f, input).unwrap();
 
@@ -140,19 +185,65 @@ mod tests {
   #[test]
   fn test_r1cs_build_template() {
     let f = &Field::new(&3911u16);
-    let input = "3 * x + 4 = 10";
-    println!("Equation: {}", input);
+    let input = "3 * x + 4";
     let (_, eq) = Parser::parse(f, input).unwrap();
 
     let gates = &Gate::build(f, &eq);
-    for gate in gates {
-      println!("{:?}", gate);
-    }
+    let r1cs = R1CS::from_gates(f, gates);
 
-    let r1cs = R1CS::build_template(gates);
+    // expected w: [1, 3, x, t1, 4, t2, out]
+    let h = r1cs.w_index;
+    let w = [
+      Term::One,
+      Term::Num(f.elem(&3u8)),
+      Term::Var("x".to_string()),
+      Term::TmpVar(1),
+      Term::Num(f.elem(&4u8)),
+      Term::TmpVar(2),
+      Term::Out,
+    ];
+    assert_eq!(h.len(), w.len());
 
-    for (i, term) in r1cs.w_tmpl.iter().enumerate() {
-      println!("{}: {:?}", i, term);
+    for (i, term) in w.iter().enumerate() {
+      assert_eq!(h.get(&term).unwrap(), &i);
     }
   }
-}
+
+  fn term_to_str(r1cs: &R1CS, vec: &SparseVec) -> String {
+    let indices = vec.indices();
+    let s = indices.iter().map(|i| {
+      match &r1cs.w_tmpl[*i] {
+        Term::Num(n) => n.to_string(),
+        Term::Var(s) => s.clone(),
+        Term::TmpVar(i) => format!("t{}", i),
+        Term::One => "1".to_string(),
+        Term::Out => "out".to_string(),
+        _ => "?".to_string(),
+      }
+    }).collect::<Vec<String>>().join(" + ");
+    format!("{}", s)
+  }
+
+  #[test]
+  fn test_r1cs_build_a_b_c_matrix() {
+    let f = &Field::new(&3911u16);
+    let input = "3 * x + 4";
+    let (_, eq) = Parser::parse(f, input).unwrap();
+
+    let gates = &Gate::build(f, &eq);
+    let r1cs = R1CS::from_gates(f, gates);
+
+    let mut res = vec![];
+    for abc in &r1cs.abcs {
+      let a = term_to_str(&r1cs, &abc.0);
+      let b = term_to_str(&r1cs, &abc.1);
+      let c = term_to_str(&r1cs, &abc.2);
+      res.push((a, b, c));
+    }
+
+    assert_eq!(res.len(), 3);
+    assert_eq!(res[0], ("3".to_string(), "x".to_string(), "t1".to_string()));
+    assert_eq!(res[1], ("4 + t1".to_string(), "1".to_string(), "t2".to_string()));  // not "t1 + 4" due to the term order in w_index
+    assert_eq!(res[2], ("t2".to_string(), "1".to_string(), "out".to_string()));
+  }
+ }
