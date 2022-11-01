bba59be3d5238a24fddef553e86a42abc3bc50bb
exfinen <47593166+exfinen@users.noreply.github.com>
2022-11-01 19:07:09 +0900

    add tests for spase matrix multiplication. add equality and pretty print to sparse matrix

--- src/snarks/sparse_matrix.rs
+++ src/snarks/sparse_matrix.rs
@@ -30,6 +30,16 @@ impl SparseMatrix {
     }
   }
 
+  pub fn pretty_print(&self) -> String {
+    let mut s = String::new();
+    let mut keys: Vec<&FieldElem> = self.rows.keys().collect();
+    keys.sort();
+    for key in keys {
+      s = format!("{}{}\n", s, self.rows.get(key).unwrap().pretty_print());
+    }
+    s
+  }
+
   pub fn set(&mut self, x: &impl ToBigUint, y: &impl ToBigUint, v: &impl ToBigUint) -> () {
     let v = self.f.elem(v);
     let x = self.f.elem(x);
@@ -60,7 +70,8 @@ impl SparseMatrix {
     }
   }
 
-  pub fn get_row(&self, y: &FieldElem) -> SparseVec {
+  pub fn get_row(&self, y: &impl ToBigUint) -> SparseVec {
+    let y = &self.f.elem(y);
     assert!(y < &self.height);
     let mut row = SparseVec::new(&self.f, &self.width);
 
@@ -77,7 +88,8 @@ impl SparseMatrix {
     row
   }
 
-  pub fn get_column(&self, x: &FieldElem) -> SparseVec {
+  pub fn get_column(&self, x: &impl ToBigUint) -> SparseVec {
+    let x = &self.f.elem(x);
     assert!(x < &self.width);
     let mut col = SparseVec::new(&self.f, &self.height);
 
@@ -89,6 +101,26 @@ impl SparseMatrix {
       }
     }
     col
+  }
+}
+
+impl PartialEq for SparseMatrix {
+  fn eq(&self, other: &SparseMatrix) -> bool {
+    if self.width != other.width || self.height != other.height {
+      return false;
+    }
+    for key in self.rows.keys() {
+      if !other.rows.contains_key(key) {
+        return false;
+      }
+      let self_row = &self.rows[key];
+      let other_row = &other.rows[key];
+
+      if self_row != other_row {
+        return false;
+      }
+    }
+    true
   }
 }
 
@@ -116,15 +148,15 @@ impl Mul<&SparseMatrix> for &SparseMatrix {
 
     fn mul(self, rhs: &SparseMatrix) -> Self::Output {
       if self.width != rhs.height {
-        panic!("Can only multiply matrix with height {:?}, but the height is {:?}",
+        panic!("Can only multiply matrix with height {:?}, but the rhs height is {:?}",
           self.width.n, rhs.height.n);
       }
-      let mut res = SparseMatrix::new(&self.f, &rhs.height, &self.height);
+      let mut res = SparseMatrix::new(&self.f, &rhs.width, &self.height);
 
       let mut y = self.f.elem(&0u8);
       while y < self.height {
         let mut x = self.f.elem(&0u8);
-        while x < self.height {
+        while x < rhs.width {
           let lhs = &self.get_row(&y);
           let rhs = &rhs.get_column(&x);
           let v = (lhs * rhs).sum();
@@ -244,8 +276,38 @@ mod tests {
     let _ = SparseMatrix::from(&vec![]);
   }
 
-  #[test]
-  fn test_from_sparse_vecs() {
+  fn gen_test_2x2_identity_matrix() -> SparseMatrix {
+    let f = &Field::new(&3911u16);
+    let zero = &f.elem(&0u8);
+    let one = &f.elem(&1u8);
+
+    let mut v1 = SparseVec::new(f, &2u8);
+    let mut v2 = SparseVec::new(f, &2u8);
+    v1.set(zero, one);
+    v2.set(one, one);
+    let vecs = vec![v1, v2];
+    SparseMatrix::from(&vecs)
+  }
+
+  fn gen_test_2x2_matrix() -> SparseMatrix {
+    let f = &Field::new(&3911u16);
+    let zero = &f.elem(&0u8);
+    let one = &f.elem(&1u8);
+    let two = &f.elem(&2u8);
+    let three = &f.elem(&3u8);
+    let four = &f.elem(&4u8);
+
+    let mut v1 = SparseVec::new(f, &2u8);
+    let mut v2 = SparseVec::new(f, &2u8);
+    v1.set(zero, one);
+    v1.set(one, two);
+    v2.set(zero, three);
+    v2.set(one, four);
+    let vecs = vec![v1, v2];
+    SparseMatrix::from(&vecs)
+  }
+
+  fn gen_test_2x3_matrix() -> SparseMatrix {
     let f = &Field::new(&3911u16);
     let zero = &f.elem(&0u8);
     let one = &f.elem(&1u8);
@@ -259,7 +321,60 @@ mod tests {
     v2.set(one, two);
     v3.set(zero, three);
     let vecs = vec![v1, v2, v3];
-    let m = SparseMatrix::from(&vecs);
+    SparseMatrix::from(&vecs)
+  }
+
+  #[test]
+  fn test_eq() {
+    let m1 = gen_test_2x2_identity_matrix();
+    let m2 = gen_test_2x2_matrix();
+    let m3 = gen_test_3x2_matrix();
+
+    assert!(&m1 == &m1);
+    assert!(&m2 == &m2);
+    assert!(&m3 == &m3);
+  }
+
+  #[test]
+  fn test_non_eq() {
+    let m1 = gen_test_2x2_identity_matrix();
+    let m2 = gen_test_2x2_matrix();
+    let m3 = gen_test_3x2_matrix();
+
+    assert!(&m1 != &m2);
+    assert!(&m1 != &m3);
+    assert!(&m2 != &m1);
+    assert!(&m2 != &m3);
+    assert!(&m3 != &m1);
+    assert!(&m3 != &m2);
+  }
+
+  fn gen_test_3x2_matrix() -> SparseMatrix {
+    let f = &Field::new(&3911u16);
+    let zero = &f.elem(&0u8);
+    let one = &f.elem(&1u8);
+    let two = &f.elem(&2u8);
+    let three = &f.elem(&3u8);
+
+    let mut v1 = SparseVec::new(f, &3u8);
+    let mut v2 = SparseVec::new(f, &3u8);
+    v1.set(zero, one);
+    v1.set(one, two);
+    v1.set(two, three);
+    v2.set(zero, three);
+    v2.set(one, two);
+    v2.set(two, one);
+    let vecs = vec![v1, v2];
+    SparseMatrix::from(&vecs)
+  }
+
+  #[test]
+  fn test_from_sparse_vecs() {
+    let m = gen_test_2x3_matrix();
+    let zero = &m.f.elem(&0u8);
+    let one = &m.f.elem(&1u8);
+    let two = &m.f.elem(&2u8);
+    let three = &m.f.elem(&3u8);
 
     assert_eq!(&m.width, two);
     assert_eq!(&m.height, three);
@@ -270,32 +385,98 @@ mod tests {
   }
 
   #[test]
+  #[should_panic]
   fn test_get_row_x_out_of_range() {
-    let f = &Field::new(&3911u16);
+    std::panic::set_hook(Box::new(|_| {}));  // suppress stack trace
+    let m = gen_test_2x3_matrix();
+    let _ = m.get_row(&m.height);
   }
 
   #[test]
   fn test_get_row_x_within_range() {
-    let f = &Field::new(&3911u16);
+    let m = gen_test_2x3_matrix();
+    let zero = &m.f.elem(&0u8);
+    let one = &m.f.elem(&1u8);
+    let two = &m.f.elem(&2u8);
+    let three = &m.f.elem(&3u8);
+
+    let r0 = m.get_row(&0u8);
+    assert_eq!(r0.get(&0u8), one);
+    assert_eq!(r0.get(&1u8), zero);
+
+    let r1 = m.get_row(&1u8);
+    assert_eq!(r1.get(&0u8), zero);
+    assert_eq!(r1.get(&1u8), two);
+
+    let r2 = m.get_row(&2u8);
+    assert_eq!(r2.get(&0u8), three);
+    assert_eq!(r2.get(&1u8), zero);
   }
 
   #[test]
+  #[should_panic]
   fn test_get_column_out_of_range() {
-    let f = &Field::new(&3911u16);
+    std::panic::set_hook(Box::new(|_| {}));  // suppress stack trace
+    let m = gen_test_2x3_matrix();
+    let _ = m.get_column(&m.width);
   }
 
   #[test]
   fn test_get_column_within_range() {
-    let f = &Field::new(&3911u16);
+    let m = gen_test_2x3_matrix();
+    let zero = &m.f.elem(&0u8);
+    let one = &m.f.elem(&1u8);
+    let two = &m.f.elem(&2u8);
+    let three = &m.f.elem(&3u8);
+
+    let c0 = m.get_column(&0u8);
+    assert_eq!(c0.get(&0u8), one);
+    assert_eq!(c0.get(&1u8), zero);
+    assert_eq!(c0.get(&2u8), three);
+
+    let c1 = m.get_column(&1u8);
+    assert_eq!(c1.get(&0u8), zero);
+    assert_eq!(c1.get(&1u8), two);
+    assert_eq!(c1.get(&2u8), zero);
   }
 
   #[test]
+  #[should_panic]
   fn test_mul_incompatible_matrices() {
-    let f = &Field::new(&3911u16);
+    std::panic::set_hook(Box::new(|_| {}));  // suppress stack trace
+    let m = gen_test_2x3_matrix();
+    let _ = &m * &m;
   }
 
   #[test]
-  fn test_mul_compatible_matrices() {
-    let f = &Field::new(&3911u16);
+  fn test_mul_different_sizes() {
+    let m1 = gen_test_3x2_matrix();
+    let m2 = gen_test_2x3_matrix();
+    let m3 = &m1 * &m2;
+
+    let two = &m3.f.elem(&2u8);
+    let six = &m3.f.elem(&6u8);
+    let four = &m3.f.elem(&4u8);
+    let ten = &m3.f.elem(&10u8);
+
+    assert_eq!(&m3.width, two);
+    assert_eq!(&m3.height, two);
+    assert_eq!(m3.get(&0u8, &0u8), ten);
+    assert_eq!(m3.get(&1u8, &0u8), four);
+    assert_eq!(m3.get(&0u8, &1u8), six);
+    assert_eq!(m3.get(&1u8, &1u8), four);
+  }
+
+  #[test]
+  fn test_mul_identity() {
+    let m1 = gen_test_2x2_matrix();
+    let m2 = gen_test_2x2_identity_matrix();
+    let m3 = &m1 * &m2;
+
+    let two = &m3.f.elem(&2u8);
+    assert_eq!(&m3.width, two);
+    assert_eq!(&m3.height, two);
+
+    assert!(&m1 == &m3);
   }
 }
