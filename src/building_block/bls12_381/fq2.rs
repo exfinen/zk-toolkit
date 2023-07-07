936ddd21e3eef6abaf6ac74d41753ff904b0a4eb
exfinen <47593166+exfinen@users.noreply.github.com>
2023-07-07 15:16:25 +0900

    wip

--- src/building_block/bls12_381/fq2.rs
+++ src/building_block/bls12_381/fq2.rs
@@ -107,8 +107,8 @@ mod tests {
   use crate::building_block::bls12_381::fq_test_helper::get_fq1_values;
   use super::*;
 
-  fn to_str_tuple(x: &Fq2) -> (String, String) {
-    (x.u1.n.to_string(), x.u0.n.to_string())
+  fn to_strs(x: &Fq2) -> [String; 2] {
+    [x.u1.n.to_string(), x.u0.n.to_string()]
   }
 
   #[test]
@@ -117,7 +117,7 @@ mod tests {
     let a2 = Fq2::new(&a1, &b1);
     let b2 = Fq2::new(&c1, &d1);
     let sum = a2 + b2;
-    let (u1, u0) = to_str_tuple(&sum);
+    let [u1, u0] = to_strs(&sum);
     assert_eq!(u1, "4002409555221667393417789825735904156556882819939007885332058136124031650490837864442687629129015664037894272559777");
     assert_eq!(u0, "4002409555221667393417789825735904156556882819939007885332058136124031650490837864442687629129015664037894272559773");
   }
@@ -128,7 +128,7 @@ mod tests {
     let a2 = Fq2::new(&a1, &b1);
     let b2 = Fq2::new(&c1, &d1);
     let sum = a2 - b2;
-    let (u1, u0) = to_str_tuple(&sum);
+    let [u1, u0] = to_strs(&sum);
     assert_eq!(u1, "4");
     assert_eq!(u0, "4");
   }
@@ -139,7 +139,7 @@ mod tests {
     let a2 = Fq2::new(&a1, &b1);
     let b2 = Fq2::new(&c1, &d1);
     let sum = a2 * b2;
-    let (u1, u0) = to_str_tuple(&sum);
+    let [u1, u0] = to_strs(&sum);
     assert_eq!(u1, "62");
     assert_eq!(u0, "24");
   }
@@ -151,13 +151,13 @@ mod tests {
     let b2 = Fq2::new(&c1, &d1);
     {
       let x = Fq2::inv(&a2);
-      let (u1, u0) = to_str_tuple(&x);
+      let [u1, u0] = to_strs(&x);
       assert_eq!(u1, "3178384058558382930067068391025570947853995180539800379528399108098495722448606539410369587837747733206563098797478");
       assert_eq!(u0, "2707512346179363236723798999762523400023773672311681804783451092083903763567331496534759278528451772731516713790444");
     }
     {
       let x = Fq2::inv(&b2);
-      let (u1, u0) = to_str_tuple(&x);
+      let [u1, u0] = to_strs(&x);
       assert_eq!(u1, "1816478182754449047781919997833987271052739125972318963343011000240906672145841800016296693220091724447967400623288");
       assert_eq!(u0, "523392018759756505293095592596233620472823137992024108081884525493142600448801874580966843809178971451109251027049");
     }
@@ -169,7 +169,7 @@ mod tests {
     let a2 = Fq2::new(&a1, &b1);
     let b2 = Fq2::new(&c1, &d1);
     let x = Fq2::apply_reduce_rule(&(&a2 * &b2));
-    let (u1, u0) = to_str_tuple(&x);
+    let [u1, u0] = to_strs(&x);
     assert_eq!(u1, "86");
     assert_eq!(u0, "4002409555221667393417789825735904156556882819939007885332058136124031650490837864442687629129015664037894272559749");
   }
