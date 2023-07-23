40b7d01cc6508ced61696ef91efa5bebf785a5a4
exfinen <47593166+exfinen@users.noreply.github.com>
2023-07-23 10:57:38 +0900

    clean up

--- src/building_block/elliptic_curve/affine_point.rs
+++
@@ -1,8 +0,0 @@
-use std::ops::{Add, Mul, Sub, Div};
-
-pub trait AffinePoint {
-  type Element: Add<Self::Element> + Sub<Self::Element> + Mul<Self::Element> + Div<Self::Element>;
-
-  fn x(&self) -> Self::Element;
-  fn y(&self) -> Self::Element;
-}
--- src/building_block/elliptic_curve/weierstrass/adder/mod.rs
+++
@@ -1,3 +0,0 @@
-pub mod affine_point_adder;
-pub mod jacobian_point_adder;
-pub mod point_adder;
